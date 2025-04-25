e48f6e08d00c2bb8315faa235eb038deb7cef17c
Eli and Willem <manjarrezenator@gmail.com>
2025-04-25 09:18:58 -0700

    Initial Commit

---
+++ FallacyDetectorNonVibeTests/FallacyDetectorNonVibeTests.swift
@@ -0,0 +1,16 @@
+//
+//  FallacyDetectorNonVibeTests.swift
+//  FallacyDetectorNonVibeTests
+//
+//  Created by Eli Manjarrez on 4/25/25.
+//
+
+import Testing
+
+struct FallacyDetectorNonVibeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
